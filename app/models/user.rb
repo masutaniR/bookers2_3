c896ac4ce8cd0cc046d969882c90a213e950b2fc
masutaniR <popohu87@gmail.com>
2021-01-18 10:08:28 +0000

    [Add] ユーザープロフィール機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  attachment :profile_image
+  validates :name,
+    uniqueness: true,
+    length: { in: 2..20 }
+  validates :introduction,
+    length: { maximum: 50 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
   get '/home/about' => 'homes#about'
+  resources :users, only: [:index, :show, :edit, :update]
 end
