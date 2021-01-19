387e4019f1a1d1128961dcd10505b835452eba4a
masutaniR <popohu87@gmail.com>
2021-01-19 02:46:16 +0000

    [Add] 部分テンプレート、エラーメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,7 @@ class BooksController < ApplicationController
       redirect_to book_path(@book), notice: 'You have created book successfully.'
     else
       @books = Book.all
+      @user = current_user
       render :index
     end
   end
@@ -13,20 +14,28 @@ class BooksController < ApplicationController
   def index
     @book = Book.new
     @books = Book.all
+    @user = current_user
   end
 
   def show
+    @new_book = Book.new
     @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def edit
-    @book = Book.find(params[:id])
+      @book = Book.find(params[:id])
+    if @book.user == current_user
+      render :edit
+    else
+      redirect_to books_path
+    end
   end
 
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_path(@book)
+      redirect_to book_path(@book), notice: 'You have updated book successfully.'
     else
       render :edit
     end
