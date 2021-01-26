aa528d12fce95168e44ace2a4cf42a5bf921862c
masutaniR <popohu87@gmail.com>
2021-01-26 07:20:16 +0000

    [Update] コメントの非同期

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,21 +1,14 @@
 class CommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     @comment = current_user.comments.new(comment_params)
-    @comment.book_id = book.id
-    if @comment.save
-      redirect_to book_path(book)
-    else
-      @new_book = Book.new
-      @book = Book.find(params[:book_id])
-      @user = @book.user
-      render 'books/show'
-    end
+    @comment.book_id = @book.id
+    @comment.save
   end
 
   def destroy
+    @book = Book.find(params[:book_id])
     Comment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
   end
 
   private
